5a4cc380f2d544939f1bb3677e6ab0a432689f09
Chilaka, Madhuri (ITOP AI) - AF (ext) <machilaka-ext@airfrance.fr>
2024-02-14 15:01:11 +0530

    Added terraform and workflow yaml files

--- iac/terraform/apps.tf
+++ iac/terraform/apps.tf
@@ -1,5 +1,5 @@
 ############################################
-## create terraform resource modules for creating and azure app service plan and app service ##
+## create terraform resource modules for creating an azure app service plan and app service ##
 ## Define variables for azure subscription id, resource group, SKU, Size and location ##
 ############################################
 
